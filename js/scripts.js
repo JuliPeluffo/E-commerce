0eb600f57adae10105a9e996438a6b8af29d6177
Juli.Peluffo <jpeluffo54@gmail.com>
2021-06-08 18:07:47 -0300

    Probando el agregar carito

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.1 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+//Que se reconozca cual es el botón que se clickea (agregar carrito)
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+  addToCartButton.addEventListener('click', addToCartClicked);
+});
+//Click en agregar al carrito, y toma el nombre, precio e imagen del item.
+function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest(".item")
+    console.log("addToCartClicked -> item" , item)
+
+    const itemTitle = item.querySelector("item-title").textContent;
+    const itemPrice = item.querySelector("item-price").texContent;
+    const itemImage = item.querySelector("item-image").src;
+
+    console.log("addToCartClicked -> itemTitle" , itemTitle, itemPrice, itemImage);
+}
